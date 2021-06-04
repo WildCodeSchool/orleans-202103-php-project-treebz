1be67a6ca11570ffdf15fca89408a665c08d6d21
Gersey Stelmach <gerseystelmach@gmail.com>
2021-06-04 10:12:16 +0200

    Corrections done

--- src/Controller/RuleController.php
+++ src/Controller/RuleController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 * @Route("/lesreglesdujeu", name="gamerules_")
 */
 
-class RulesController extends AbstractController
+class RuleController extends AbstractController
 {
     /**
     * @Route("/", name="index")
