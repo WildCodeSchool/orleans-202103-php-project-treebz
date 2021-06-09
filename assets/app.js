7c2dd47139c75f6757fffe4a0b0b2c584532cf64
Valentin <valentin.lay98@gmail.com>
2021-06-09 10:16:00 +0200

    Corecction librairie iconify

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import Iconify from '@iconify/iconify';
 
 // start the Stimulus application
 import './bootstrap';
