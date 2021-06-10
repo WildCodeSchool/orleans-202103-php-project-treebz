aa8add4d69b91793a81473b36e32f0abb951d599
Gersey Stelmach <gerseystelmach@gmail.com>
2021-06-10 17:09:10 +0200

    Page Creer Jeu, with the new step bar and the formulaire (just view)

--- src/Controller/GameCreationController.php
+++ src/Controller/GameCreationController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Command;
+use App\Form\CommandType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 
 /**
 * @Route("/creervotrejeu", name="gamecreation_")
@@ -18,7 +22,8 @@ class GameCreationController extends AbstractController
 
     public function index(): Response
     {
-
-        return $this->render('gameCreation/index.html.twig');
+        $commandForm = new Command();
+        $form = $this->createForm(CommandType::class, $commandForm);
+        return $this->render('gameCreation/index.html.twig', ["form" => $form->createView(),]);
     }
 }
