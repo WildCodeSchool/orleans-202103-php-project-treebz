ff06229d2ae7394de5e7b3765cac317bcc6d583b
Valentin <valentin.lay98@gmail.com>
2021-06-24 16:06:19 +0200

    Rajout du texte dans la template

--- src/Controller/TermController.php
+++ src/Controller/TermController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TermController extends AbstractController
 {
+    /**
+     * @Route("/condition_generale_d\'utilisation", name="termOfService")
+     */
     public function termOfService(): Response
     {
         return $this->render('terms/termOfService.html.twig');
