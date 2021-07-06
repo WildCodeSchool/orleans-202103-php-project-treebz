ccccaed91a9c88e71c6c3c2c8b41bff782fc674e
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-07-06 10:22:13 +0200

    Cards Preview

---
+++ migrations/Version20210706073059.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210706073059 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE command (id INT AUTO_INCREMENT NOT NULL, project_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE command_theme (command_id INT NOT NULL, theme_id INT NOT NULL, INDEX IDX_4DDB23B33E1689A (command_id), INDEX IDX_4DDB23B59027487 (theme_id), PRIMARY KEY(command_id, theme_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `member` (id INT AUTO_INCREMENT NOT NULL, command_id INT DEFAULT NULL, name VARCHAR(15) NOT NULL, picture VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_70E4FA7833E1689A (command_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE theme (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(500) NOT NULL, color_text VARCHAR(7) NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, user_detail_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D649D8308E5F (user_detail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_detail (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, address VARCHAR(500) NOT NULL, postal_code VARCHAR(5) NOT NULL, town VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, phone VARCHAR(35) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE command_theme ADD CONSTRAINT FK_4DDB23B33E1689A FOREIGN KEY (command_id) REFERENCES command (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE command_theme ADD CONSTRAINT FK_4DDB23B59027487 FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE `member` ADD CONSTRAINT FK_70E4FA7833E1689A FOREIGN KEY (command_id) REFERENCES command (id)');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649D8308E5F FOREIGN KEY (user_detail_id) REFERENCES user_detail (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE command_theme DROP FOREIGN KEY FK_4DDB23B33E1689A');
+        $this->addSql('ALTER TABLE `member` DROP FOREIGN KEY FK_70E4FA7833E1689A');
+        $this->addSql('ALTER TABLE command_theme DROP FOREIGN KEY FK_4DDB23B59027487');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649D8308E5F');
+        $this->addSql('DROP TABLE command');
+        $this->addSql('DROP TABLE command_theme');
+        $this->addSql('DROP TABLE `member`');
+        $this->addSql('DROP TABLE theme');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE user_detail');
+    }
+}
--- src/Controller/PreviewController.php
+++ src/Controller/PreviewController.php
@@ -2,12 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Theme;
-use App\Entity\Member;
 use App\Entity\Command;
 use App\Repository\ThemeRepository;
 use App\Repository\MemberRepository;
-use App\Repository\CommandRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,50 +27,8 @@ class PreviewController extends AbstractController
         ThemeRepository $themeRepository
     ): Response {
 
-        $themeNumber = '10000';
-        $memberNumber = '10000';
         return $this->render('gameCreation/preview.html.twig', [
             'command' => $command,
-            'viewTheme' => $themeNumber,
-            'memberNumber' => $memberNumber,
-        ]);
-    }
-
-    /**
-     * @Route("/prévisualisation-du-jeu/{command_id}/une-carte", name="preview_one_card", methods={"GET","POST"})
-     * @ParamConverter("command", class="App\Entity\Command", options={"mapping": {"command_id": "id"}})
-     */
-
-    public function oneCard(
-        MemberRepository $memberRepository,
-        Command $command,
-        ThemeRepository $themeRepository
-    ): Response {
-
-        $themeNumber = '1';
-        $memberNumber = '1';
-        return $this->render('gameCreation/preview.html.twig', [
-            'command' => $command,
-            'viewTheme' => $themeNumber,
-            'memberNumber' => $memberNumber,
-        ]);
-    }
-
-    /**
-     * @Route("/prévisualisation-du-jeu/{command_id}/une-famille", name="preview_one_familly", methods={"GET","POST"})
-     * @ParamConverter("command", class="App\Entity\Command", options={"mapping": {"command_id": "id"}})
-     */
-
-    public function oneFamilly(
-        Command $command,
-        CommandRepository $commandRepository
-    ): Response {
-        $themeNumber = '1';
-        $memberNumber = '1000';
-        return $this->render('gameCreation/preview.html.twig', [
-            'command' => $command,
-            'viewTheme' => $themeNumber,
-            'memberNumber' => $memberNumber,
         ]);
     }
 }
