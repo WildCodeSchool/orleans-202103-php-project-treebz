8eb8258f5ab127736b0da58a908b55cc82347063
Valentin <valentin.lay98@gmail.com>
2021-07-07 12:18:52 +0200

    ajout d'un méthode change status

--- src/Controller/Admin/CommandController.php
+++ src/Controller/Admin/CommandController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Command;
+use App\Entity\Status;
+use App\Form\ChangeStatusOrderType;
 use App\Repository\CommandRepository;
 use App\Repository\StatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,9 +22,25 @@ class CommandController extends AbstractController
      */
     public function index(CommandRepository $commandRepository, StatusRepository $statusRepository): Response
     {
+
+
         return $this->render('admin/command/index.html.twig', [
             'commands' => $commandRepository->findAll(),
-            'status' => $statusRepository->findAll()
+            'status' => $statusRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="change_status", methods={"POST"})
+     */
+    public function changeStatus(Request $request, Command $command, StatusRepository $statusRepository): Response
+    {
+        if ($this->isCsrfTokenValid('change_status' . $command->getId(), $request->request->get('_token'))) {
+            $status = $statusRepository->find($request->request->get('status'));
+            $command->setStatus($status);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        return $this->redirectToRoute('command_index');
     }
 }
