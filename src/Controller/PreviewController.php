bd6c0e6cbc9ea44d1715c4bfa6400171769a439a
aurelien <vannier.autrelien2@gmail.com>
2021-07-13 00:10:48 +0200

    recapitulatif commande

--- src/Controller/PreviewController.php
+++ src/Controller/PreviewController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Command;
+use App\Service\GameCard;
 use App\Repository\ThemeRepository;
 use App\Repository\MemberRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/creez-votre-jeu", name="gamecreation_")
@@ -26,7 +27,8 @@ class PreviewController extends AbstractController
     public function index(
         MemberRepository $memberRepository,
         Command $command,
-        ThemeRepository $themeRepository
+        ThemeRepository $themeRepository,
+        GameCard $gameCard
     ): Response {
 
          /** @var User */
@@ -34,9 +36,10 @@ class PreviewController extends AbstractController
         if (!$user->getCommands()->contains($command)) {
             throw $this->createAccessDeniedException("Vous n'avez pas accès à cette commande");
         }
-
+        $priceGame = $gameCard->priceGame($command);
         return $this->render('gameCreation/preview.html.twig', [
             'command' => $command,
+            'priceGame' => $priceGame,
         ]);
     }
 }
